0e9f40fc17aaf15c33316b81198aa79d0b8ca9b6
Isadora Torqueti <isadtorqueti@gmail.com>
2025-08-24 18:12:09 -0300

    final adjustments

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from st_pages import get_nav_from_toml
 
-st.set_page_config(page_title="Instagram Downloader", layout="wide")
+st.set_page_config(page_title="PFP/Thumb Downloader", layout="wide", page_icon="🖼️")
 
 nav = get_nav_from_toml(".streamlit/pages.toml")
 pg = st.navigation(nav)
